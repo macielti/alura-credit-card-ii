5cbc3aabc6937c92d47a5f17e5ec60b4612d38ae
Bruno do Nascimento Maciel <brunodonascimentomaciel@gmail.com>
2021-05-22 18:09:46 -0300

    obrgatories

---
+++ doc/intro.md
@@ -0,0 +1,3 @@
+# Introduction to alura-credit-card-ii
+
+TODO: write [great documentation](http://jacobian.org/writing/what-to-write/)
---
+++ project.clj
@@ -0,0 +1,14 @@
+(defproject alura-credit-card-ii "0.1.0-SNAPSHOT"
+  :description "FIXME: write description"
+  :url "http://example.com/FIXME"
+  :license {:name "EPL-2.0 OR GPL-2.0-or-later WITH Classpath-exception-2.0"
+            :url "https://www.eclipse.org/legal/epl-2.0/"}
+  :repositories {"my.datomic.com" {:url "https://my.datomic.com/repo"
+                                   :creds :gpg}}
+  :dependencies [[org.clojure/clojure "1.10.1"]
+                 [com.datomic/datomic-pro "1.0.6269"]
+                 [clojure.java-time "0.3.2"]]
+  :main ^:skip-aot alura-credit-card-ii.core
+  :target-path "target/%s"
+  :profiles {:uberjar {:aot :all
+                       :jvm-opts ["-Dclojure.compiler.direct-linking=true"]}})
